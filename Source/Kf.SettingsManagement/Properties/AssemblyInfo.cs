343b409a0caa70548dec926dc93401b66c8d1813
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-16 10:38:21 +0200

    Added project (profile 111 and net45 minimum). Edited AssemblyInfo.

---
+++ Source/Kf.SettingsManagement/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Kf.SettingsManagement")]
+[assembly: AssemblyDescription("Basic .Net library for supporting settings and settings management.")]
+[assembly: AssemblyCompany("KodeFoxx")]
+[assembly: AssemblyProduct("Kf.SettingsManagement")]
+[assembly: AssemblyCopyright("Copyright © Yves Schelpe as KodeFoxx 2015")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("1.0.*")]
