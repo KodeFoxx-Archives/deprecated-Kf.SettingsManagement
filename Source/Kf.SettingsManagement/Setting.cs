18fec5391e7996ba96d4d0d0ccb57f7ea05b971c
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-16 10:58:18 +0200

    Added "Setting" type and "ValueType" type.

---
+++ Source/Kf.SettingsManagement/Setting.cs
@@ -0,0 +1,36 @@
+﻿namespace Kf.SettingsManagement
+{
+    /// <summary>
+    /// Defines a setting.
+    /// </summary>
+    public class Setting
+    {
+        /// <summary>
+        /// Gets the setting's key.
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Gets/sets the setting's type.
+        /// </summary>
+        public ValueType Type { get; set; }
+
+        /// <summary>
+        /// Gets/sets the setting's value as a string.
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Creates a new <see cref="Setting"/>.
+        /// </summary>
+        public Setting() {
+        }
+
+        /// <summary>
+        /// String representation this object.
+        /// </summary>        
+        public override string ToString() {
+            return this.ToDebugString();
+        }
+    }
+}
---
+++ Source/Kf.SettingsManagement/ValueType.cs
@@ -0,0 +1,13 @@
+﻿namespace Kf.SettingsManagement
+{
+    /// <summary>
+    /// Defines the different types of values.
+    /// </summary>
+    public enum ValueType
+    {
+        @String,
+        @Int32,
+        @Int64,
+        @Bool
+    }
+}
