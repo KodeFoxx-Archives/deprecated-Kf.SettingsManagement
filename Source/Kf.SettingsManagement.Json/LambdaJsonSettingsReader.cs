7c50524ed4a1a87578b89a65fafe889d243a7ef0
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-23 14:35:21 +0200

    Added lambda settings reader & writer implementation.

---
+++ Source/Kf.SettingsManagement.Json/LambdaJsonSettingsReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kf.SettingsManagement.Json
+{
+    public sealed class LambdaJsonSettingsReader : JsonSettingsReader
+    {
+        private readonly Func<string> _stringLoaderFunction;
+
+        public LambdaJsonSettingsReader(Func<string> stringLoaderFunction) {
+            if(_stringLoaderFunction == null) {
+                throw new ArgumentNullException(nameof(stringLoaderFunction));
+            }
+
+            _stringLoaderFunction = stringLoaderFunction;
+        }
+
+        protected override string LoadJsonString() {
+            return _stringLoaderFunction();
+        }
+    }
+}
---
+++ Source/Kf.SettingsManagement.Json/LambdaJsonSettingsWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kf.SettingsManagement.Json
+{
+    public sealed class LambdaJsonSettingsWriter : JsonSettingsWriter
+    {
+        private readonly Action<string> _stringSaverFunction;
+
+        public LambdaJsonSettingsWriter(Action<string> stringSaverFunction) {
+            if (_stringSaverFunction == null) {
+                throw new ArgumentNullException(nameof(stringSaverFunction));
+            }
+
+            _stringSaverFunction = stringSaverFunction;
+        }
+
+        protected override void SaveJsonString(string jsonString) {
+            _stringSaverFunction(jsonString);
+        }
+    }
+}
