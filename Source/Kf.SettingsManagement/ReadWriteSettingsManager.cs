1bf97255c63f9e73c86083cc1cc4a710f861278f
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-23 14:05:54 +0200

    Added implementations.

---
+++ Source/Kf.SettingsManagement/ReadWriteSettingsManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Kf.SettingsManagement
+{
+    public class ReadWriteSettingsManager : ReadOnlySettingsManager, IReadWriteSettingsManager
+    {
+        /// <summary>
+        /// Holds a reference to the settings writer used.
+        /// </summary>
+        private readonly ISettingsWriter _settingsWriter;
+
+        /// <summary>
+        /// Creates a new <see cref="SettingsReadWriteManager"/>.
+        /// </summary>
+        /// <param name="settingsReader">The settings reader to use.</param>
+        /// <param name="settingsWriter">The settings writer to use.</param>
+        public ReadWriteSettingsManager(ISettingsReader settingsReader, ISettingsWriter settingsWriter)
+            : base(settingsReader)
+        {
+            if (settingsWriter == null) {
+                throw new ArgumentNullException(nameof(settingsWriter));
+            }
+
+            _settingsWriter = settingsWriter;
+        }
+
+        /// <summary>
+        /// Saves the given setting.
+        /// </summary>
+        /// <param name="setting">The setting to save.</param>
+        public void Save(Setting setting) {
+            var formattedKey = FormatKey(setting.Key);
+
+            if (!CheckIfKeyExists(formattedKey)) {
+                _settings.Add(formattedKey, setting);
+            } else {
+                _settings[formattedKey] = setting;
+            }
+
+            SaveSettings();
+        }
+
+        /// <summary>
+        /// Saves the settings.
+        /// </summary>
+        protected virtual void SaveSettings() {
+            try {
+                _settingsWriter.Write(_settings.Values);
+            } catch (Exception ex) {
+                string exceptionMessage = $"Exception '{ex.ToFriendlyNameOfType()}' occurred while saving settings.";                                
+                throw new SettingsException(exceptionMessage, ex);
+            }
+        }
+    }
+}
