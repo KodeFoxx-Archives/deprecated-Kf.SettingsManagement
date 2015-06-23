33aed00a99c71d7180477e62f54343b4ea96ff20
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-23 14:52:12 +0200

    Added stream read/write settings reader and writer implementations.

---
+++ Source/Kf.SettingsManagement.Json/StreamJsonSettingsReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Kf.SettingsManagement.Json
+{
+    public sealed class StreamJsonSettingsReader : JsonSettingsReader
+    {
+        private readonly Func<Stream> _getStreamFunction;
+
+        public StreamJsonSettingsReader(Func<Stream> getStreamFunction) {
+            if(getStreamFunction == null) {
+                throw new ArgumentNullException(nameof(getStreamFunction));
+            }
+
+            _getStreamFunction = getStreamFunction;
+        }
+
+        protected override string LoadJsonString() {
+            using (var stream = _getStreamFunction()) {
+                using (var streamReader = new StreamReader(stream)) {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
---
+++ Source/Kf.SettingsManagement.Json/StreamJsonSettingsWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Kf.SettingsManagement.Json
+{
+    public sealed class StreamJsonSettingsWriter : JsonSettingsWriter
+    {
+        private readonly Func<Stream> _getStreamFunction;
+
+        public StreamJsonSettingsWriter(Func<Stream> getStreamFunction) {
+            if (getStreamFunction == null) {
+                throw new ArgumentNullException(nameof(getStreamFunction));
+            }
+
+            _getStreamFunction = getStreamFunction;
+        }
+
+        protected override void SaveJsonString(string jsonString) {
+            using (var stream = _getStreamFunction()) {
+                using (var streamWriter = new StreamWriter(stream)) {
+                    streamWriter.Write(jsonString);
+                }
+            }
+        }
+    }
+}
