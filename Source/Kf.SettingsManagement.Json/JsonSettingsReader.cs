a3ac8daac559a11ea6c316d710ff982448353aca
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-23 14:28:25 +0200

    Json based implementations of SettingsReader & SettingsWriter.

---
+++ Source/Kf.SettingsManagement.Json/JsonSettingsReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Kf.SettingsManagement.Json
+{
+    /// <summary>
+    /// Json based <see cref="ISettingsReader"/>.
+    /// </summary>
+    public abstract class JsonSettingsReader : ISettingsReader
+    {        
+        /// <summary>
+        /// Reads the settings.
+        /// </summary>
+        /// <returns>An enumerable of <see cref="Setting"/>.</returns>
+        public IEnumerable<Setting> Read() {
+            var jsonString = LoadJsonString();
+            var settings = JsonConvert.DeserializeObject<IEnumerable<Setting>>(jsonString);
+            return settings;
+        }
+
+        /// <summary>
+        /// Loads the json string.
+        /// </summary>        
+        protected abstract string LoadJsonString();        
+    }
+}
---
+++ Source/Kf.SettingsManagement.Json/JsonSettingsWriter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Kf.SettingsManagement.Json
+{
+    /// <summary>
+    /// Json based <see cref="ISettingsWriter"/>.
+    /// </summary>
+    public abstract class JsonSettingsWriter : ISettingsWriter
+    {
+        /// <summary>
+        /// Writes the settings.
+        /// </summary>
+        /// <param name="settings">The settings to write.</param>
+        public void Write(IEnumerable<Setting> settings) {
+            var jsonString = JsonConvert.SerializeObject(settings, Formatting.None);
+            SaveJsonString(jsonString);
+        }
+
+        /// <summary>
+        /// Saves the json string.
+        /// </summary>        
+        protected abstract void SaveJsonString(string jsonString);
+    }
+}
